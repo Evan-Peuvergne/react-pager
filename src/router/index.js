ed0c721e414201f382ee079a1d341d7969bec14a
Evan Peuvergne <evanpeuvergne@gmail.com>
2018-09-30 22:00:07 +0200

    Feature/basic-routing: Create basic router component

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Page from 'page'
 
 
 class Router extends Component {
 
+  constructor (props) {
+    super(props)
+
+    this.router = Page
+    this.props.routes.forEach(r => {
+      this.router(r.url, this.display.bind(this, r))
+    })
+
+    this.state = {
+      current: null
+    }
+  }
+
+  componentDidMount () {
+    this.router.start()
+  }
+
+  display (route) {
+    this.setState({ current: route.component })
+  }
+
   render () {
+    let Children = this.state.current
     return (
-      <div></div>
+      <div>
+        {Children &&
+          <Children></Children>
+        }
+      </div>
     )
   }
 
+}
+
+Router.propTypes = {
+  routes: PropTypes.array.isRequired
 }
 
 export default Router
