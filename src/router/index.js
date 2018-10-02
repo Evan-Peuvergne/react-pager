f87afedf8b84af96e1c567fb37da840f93e5636b
Evan Peuvergne <evanpeuvergne@gmail.com>
2018-10-03 00:00:51 +0200

    Feature/basic routing
    * Feature/basic-routing: Create basic router component
    
    * Feature/basic-routing: Implement basic behavior for links
    
    * Feature/basic-routing: Making Links working with styled-components
    
    * Feature/basic-routing: Implement NotFound property
    
    * Feature/basic-routing: Move "Not found" definition into routes object
    
    * Feature/basic-routing: Improve link component and remove router store

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Page from 'page'
 
 
 class Router extends Component {
 
+  constructor (props) {
+    super(props)
+
+    this.router = Page
+    this.props.routes.forEach(route => {
+      this.router(route.url, this.display.bind(this, route))
+    })
+
+    this.state = { current: null }
+  }
+
+  componentDidMount () {
+    this.router.start()
+  }
+
+  display (route) {
+    this.setState({ current: route.component })
+  }
+
   render () {
+    let Current = this.state.current
+
     return (
-      <div></div>
+      <div>
+        {Current &&
+          <Current />
+        }
+      </div>
     )
   }
 
+}
+
+Router.propTypes = {
+  routes: PropTypes.array.isRequired
 }
 
 export default Router
