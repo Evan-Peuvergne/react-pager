66c6636548312ab4a85c0474eca9179d7aaf2352
Evan Peuvergne <evanpeuvergne@gmail.com>
2019-03-03 03:20:34 +0100

    Hotfix: Remove types folder

--- types/index.d.ts
+++
@@ -1,3 +0,0 @@
-import Router from "./router";
-import Link from "./link";
-export { Router, Link };
--- types/router/styles.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const Root: import("styled-components").StyledComponent<"div", any, {}, never>;
