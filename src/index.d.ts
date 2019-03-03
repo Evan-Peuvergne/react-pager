c8af92a1b73f824fb8c4723c48c659c8d676a4a6
Evan Peuvergne <evanpeuvergne@gmail.com>
2019-03-03 02:43:45 +0100

    Env/typescript: Fix typescript declaration emit system

--- src/index.d.ts
+++
@@ -1,6 +0,0 @@
-import Router from './router';
-import Link from './link';
-import { Route } from './router';
-export default Router;
-export { Router, Link };
-export { Route };
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import Router from './router';
-import Link from './link';
-export default Router;
-export { Router, Link };
