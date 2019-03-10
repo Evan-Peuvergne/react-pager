9f4600b84a0723eceb8698ad1573bec426024e6d
Evan Peuvergne <evanpeuvergne@gmail.com>
2019-03-10 02:44:42 +0100

    Feature/history: reorganise exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import Router from './router'
 import Link from './link'
+import history from './router/history'
+import { isUrlMatchingRoute } from './router/utils'
+
+export default Router
+export { Router, Link, history }
+export { isUrlMatchingRoute }
 
 import { Route } from './router'
 import { LinkProps } from './link'
+import { HistoryChangedEvent } from './router/history'
 
-export default Router
-export { Router, Link }
-
-export { Route, LinkProps }
+export { Route, LinkProps, HistoryChangedEvent }
