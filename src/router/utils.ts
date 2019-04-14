7568414d766dade7ef898e838715e129c04c8d2f
Evan Peuvergne <evanpeuvergne@gmail.com>
2019-04-14 23:19:18 +0200

    Feature/links: Update typings

--- src/router/utils.ts
+++ src/router/utils.ts
@@ -1,4 +1,7 @@
-export const isMatching = (url: string, pattern: string | RegExp): boolean => {
+import Route, { RouteType } from './route'
+
+export const isMatching = (url: string, route: RouteType): boolean => {
+  const pattern = route.props.pattern
   if (pattern instanceof RegExp) return pattern.test(url)
   if (typeof pattern === 'string') return pattern === url
   return false
--- types/router/utils.d.ts
+++ types/router/utils.d.ts
@@ -1,1 +1,2 @@
-export declare const isMatching: (url: string, pattern: string | RegExp) => boolean;
+/// <reference types="react" />
+export declare const isMatching: (url: string, route: import("react").ReactElement<import("./route").RouteProps, string | ((props: any) => import("react").ReactElement<any, string | any | (new (props: any) => import("react").Component<any, any, any>)> | null) | (new (props: any) => import("react").Component<any, any, any>)>) => boolean;
